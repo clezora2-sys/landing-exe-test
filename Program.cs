5a9cc414953f7a997c848dfe6871ea5d1da8d3ae
clezora2-sys <clezora2@gmail.com>
2026-09-23 00:39:25 +0100

    Add C# source files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace LandingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Empty console app: exits immediately without doing anything
+        }
+    }
+}
